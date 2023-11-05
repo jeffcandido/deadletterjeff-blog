9ad3c111f28b81cec747c2202b206a1b268dcea2
Jeff <jeffersoncandidoeng@gmail.com>
2023-11-05 04:13:05 -0300

    Finishing project initialization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   output: "export",
   distDir: ".dist",
   images: { unoptimized: true },
+  trailingSlash: true,
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+  },
 };
 
 module.exports = nextConfig;
